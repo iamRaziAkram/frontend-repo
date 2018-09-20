650ba397bd0cb71cb935692b5be6bf7cb9b61a8f
Srishti Gupta <mail@srishtigupta.in>
2018-09-21 01:37:23 +0530

    Added logout functionality

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -180,7 +180,12 @@ class Header extends Component {
     }
 
     logoutHandler = (e) => {
+        sessionStorage.removeItem("uuid");
+        sessionStorage.removeItem("access-token");
 
+        this.setState({
+            loggedIn: false
+        });
     }
 
     render() {
